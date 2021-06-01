3085aaeeea54e3f601a62a862acdac09a4d1ffe0
leonildomello42 <leonildo.mello@gmail.com>
2021-06-01 09:52:16 -0300

    Tratando excecoes

---
+++ JavaCompletoNelio/ws-curso/CursoJava/src/cap15_excecoes/tryCatchFinally/application/Program.java
@@ -0,0 +1,38 @@
+package cap15_excecoes.tryCatchFinally.application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		try {
+
+			String[] vect = sc.nextLine().split(" ");
+			int position = sc.nextInt();
+			System.out.println(vect[position]);
+			
+		} 
+		catch (ArrayIndexOutOfBoundsException e) {
+			
+			System.out.println("Invalid position!");
+			
+		} 
+		catch (InputMismatchException e) {
+			
+			System.out.println("Input error");
+			
+		}
+		finally {
+			System.out.println("finally");
+		}
+		
+		System.out.println("End of program");
+		
+		sc.close();
+	}
+
+}
